aec95a118674e5d0c36274b42593b856bf852ab1
wamandaka <wamandaka122@gmail.com>
2025-03-26 16:28:28 +0700

    feat: add user role management from localStorage

--- src/atoms/authAtoms.ts
+++ src/atoms/authAtoms.ts
@@ -9,3 +9,19 @@ export const errorAtom = atom("");
 
 // State untuk mengontrol tampilan form (Login / Lupa Password)
 export const isLupaPasswordAtom = atom(false);
+
+// Ambil role dari localStorage
+const user = localStorage.getItem("user");
+let role: "admin" | "finance" | "operator" = "operator"; // Default role
+
+if (user) {
+  try {
+    const userData = JSON.parse(user);
+    if (userData && userData.role) {
+      role = userData.role; // Ambil role dari userData
+    }
+  } catch (error) {
+    console.error("Error parsing user data from localStorage:", error);
+  }
+}
+export const userRoleAtom = atom<"admin" | "finance" | "operator">(role);
