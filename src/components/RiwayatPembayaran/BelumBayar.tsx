48cc7f97e3b0aa6722205612d5e1019a70483792
wamandaka <wamandaka122@gmail.com>
2025-04-23 09:45:23 +0700

    feat: implement detailed payment history display in BelumBayar and SudahBayar components

--- src/components/RiwayatPembayaran/BelumBayar.tsx
+++ src/components/RiwayatPembayaran/BelumBayar.tsx
@@ -1,5 +1,21 @@
 const BelumBayar = () => {
-  return <div>BelumBayar</div>;
+  return (
+    <div className="px-5 overflow-y-scroll max-h-[calc(100vh-100px)] md:max-h-[calc(100vh-200px)]">
+      {Array.from({ length: 500 }, (_, index) => (
+        <div
+          key={index}
+          className="flex justify-between items-center py-4 my-2 border-b border-gray-200"
+        >
+          <p className="font-medium">Adam Levine</p>
+          <p className="text-red-500 font-medium">Rp200.000</p>
+        </div>
+      ))}
+      {/* <div className="flex justify-between items-center py-4 my-2 border-b border-gray-200">
+        <p>Adam Levine</p>
+        <p className="text-red-500 font-medium">Rp200.000</p>
+      </div> */}
+    </div>
+  );
 };
 
 export default BelumBayar;
--- src/components/RiwayatPembayaran/SudahBayar.tsx
+++ src/components/RiwayatPembayaran/SudahBayar.tsx
@@ -1,5 +1,24 @@
 const SudahBayar = () => {
-  return <div>SudahBayar</div>;
+  return (
+    <div className="px-5 overflow-y-auto max-h-[calc(100vh-100px)] md:max-h-[calc(100vh-200px)]">
+      {Array.from({ length: 500 }, (_, index) => (
+        <div
+          key={index}
+          className="flex justify-between items-center py-1 my-2 border-b border-gray-200"
+        >
+          <p className="font-medium">Baskara</p>
+          <div className="text-right">
+            <p className="text-green-500 font-medium">+Rp300.000</p>
+            <p className="text-gray-400 text-sm">01/01/2025</p>
+          </div>
+        </div>
+      ))}
+      {/* <div className="flex justify-between items-center py-4 my-2 border-b border-gray-200">
+        <p>Adam Levine</p>
+        <p className="text-red-500 font-medium">Rp200.000</p>
+      </div> */}
+    </div>
+  );
 };
 
 export default SudahBayar;
